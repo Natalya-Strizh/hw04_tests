e048f6c671e2b9a4b7b5f2162d18d93c805682a5
Наталья Иванова <natashaneti96@yandex.ru>
2022-05-24 03:03:39 +0300

    домашняя работа

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -148,10 +148,11 @@ class PaginatorViewsTest(TestCase):
             description='Тестовое описание',
             slug='test-slug',
         )
-        for post in range(POSTS_ALL): Post.objects.create(
-            author = cls.user,
-            text = 'Текст №' + str(post + 1),
-            group = cls.group,
+        for post in range(POSTS_ALL): 
+            Post.objects.create(
+                author = cls.user,
+                text = 'Текст №' + str(post + 1),
+                group = cls.group,
             )
 
     def setUp(self):
@@ -163,13 +164,14 @@ class PaginatorViewsTest(TestCase):
         dict = {
             reverse('posts:index'): FIRST_PAGE,
             reverse('posts:index') + '?page=2': SECOND_PAGE,
-            reverse('posts:group_list', kwargs={'slug': 'test-slug'}):
-            FIRST_PAGE,
-            reverse('posts:group_list', kwargs={'slug': 'test-slug'}) + '?page=2':
-            SECOND_PAGE,
+            reverse('posts:group_list', kwargs={'slug': 
+            'test-slug'}): FIRST_PAGE,
+            reverse('posts:group_list', kwargs={'slug': 
+            'test-slug'}) + '?page=2': SECOND_PAGE,
             reverse('posts:profile', kwargs={'username': 'author'}):
             FIRST_PAGE,
-            reverse('posts:profile', kwargs={'username': 'author'}) + '?page=2':
+            reverse('posts:profile', kwargs={'username': 
+            'author'}) + '?page=2':
             SECOND_PAGE,
         }
         for reverse_page, len_posts in dict.items():
